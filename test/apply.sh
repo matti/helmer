f0643cc7b24696b6c8616101f4d922a1d8fa30e0
Matti Paksula <matti.paksula@iki.fi>
2022-06-09 18:55:52 +0300

    test

--- test/apply.sh
+++ test/apply.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cat test/prometheus.yml | ./helmer apply https://prometheus-community.github.io/helm-charts prometheus 15.8.1 prometheus-1
+./helmer apply charts/my-ingress-nginx
--- test/diff.sh
+++ test/diff.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./helmer diff \
-  test/prometheus.yml \
-  https://raw.githubusercontent.com/prometheus-community/helm-charts/prometheus-15.8.1/charts/prometheus/values.yaml
+./helmer diff charts/my-ingress-nginx
